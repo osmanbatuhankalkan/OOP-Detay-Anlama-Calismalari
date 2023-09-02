b7b9e9d04c1dda4bd819cdea22856678f75d44ce
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-09-02 23:29:32 +0300

    Add files via upload

---
+++ NTP-Inheritance(Kalıtım)-1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Static_Constructor
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+            Muhasebeci muhasebeci = new Muhasebeci();
+            Mudur mudur = new Mudur();
+            Yazilimci yazilimci = new Yazilimci();
+            
+         }                
+    }
+    class Personel
+    {
+       public string Adi {get;set;}
+        public string Soyadi {get;set;}
+        public string MedeniHal {get;set;}                     
+    }
+    class Muhasebeci:Personel
+    {
+          public bool Musavir { get; set;}  //musavirlik sadece muhasebeciye özel
+    }
+    class Yazilimci:Personel
+    {
+          public string[] kullandigiDiller{get;set;}
+    }
+    class Mudur:Personel
+    {
+ 
+    }
+    //*Normalde böyle tek tek yazmak kod tekrarına sebep olur, verimi düşürür , esnek olmaz vs
+    /*
+    class Muhasebeci
+    {
+        public string Adi {get;set;}
+        public string Soyadi {get;set;}
+        public string MedeniHal {get;set;}                 
+    }
+    class Yazilimci
+    {
+        public string Adi {get;set;}
+        public string Soyadi {get;set;}
+        public string MedeniHal {get;set;}   
+    }
+    class Mudur
+    {
+        public string Adi {get;set;}
+        public string Soyadi {get;set;}
+        public string MedeniHal {get;set;}   
+    }
+    */                     
+}
