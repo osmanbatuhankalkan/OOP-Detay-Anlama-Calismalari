e03d24a337191109953c8bd0ab9bfc45f6f8ca50
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-09-07 18:58:35 +0300

    Add files via upload

---
+++ NTP-Inheritance(Kalıtım)-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace kalitim2
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+
+         }                
+    }
+    class BuyukBaba // Baba ve Hala BuyukBaba'nın Derived classdır
+    {
+
+    }
+    class Baba : BuyukBaba // Burada "BuyukBaba" base class // Anne Baba'nın Derived classdır
+    {
+
+    }
+    class Hala : BuyukBaba // Burada "BuyukBaba" base class
+    {
+
+    }
+    class Anne:Baba //*aşşağıda bir sınıf sadece 1 sınıftan kalıtım aldığı için "Anne" yi oğlana tanımlayamıyoruz. Tanımlayabilmek için "Baba" dan kalıtım "Anne" , "Anne"den kalıtım "Ogul" yapıyoruz.
+    {
+
+    } 
+    class Ogul : Anne // Anne, Baba olamaz // Burada "Anne" base class
+    {
+
+    }                     
+}
