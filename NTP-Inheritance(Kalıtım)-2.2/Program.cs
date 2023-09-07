37d2daef3745e61ac37c3a73c0aaaaa74a4401e1
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-09-08 01:09:09 +0300

    Add files via upload

---
+++ NTP-Inheritance(Kalıtım)-2.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace kalitim2_2
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+             new D();            
+         }                
+    }
+    class A
+    {
+         public A()
+         {
+            System.Console.WriteLine($"{nameof(A)} Nesnesi Oluşturulmuştur. ");             
+         }                
+    }
+    class B : A
+    {
+            public B()
+            {
+              System.Console.WriteLine($"{nameof(B)} Nesnesi Oluşturulmuştur. ");
+            }
+    }
+    class C : B
+    {
+              public C()
+              {
+                System.Console.WriteLine($"{nameof(C)} Nesnesi Oluşturulmuştur. ");
+              }
+    }
+    class D : C
+    {
+               public D()
+               {
+                 System.Console.WriteLine($"{nameof(D)} Nesnesi Oluşturulmuştur. ");
+               }
+    } 
+
+}
