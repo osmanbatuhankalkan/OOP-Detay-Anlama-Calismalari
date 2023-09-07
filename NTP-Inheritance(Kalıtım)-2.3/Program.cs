0aeb710567af3922f3bd04200d2c97084039ba43
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-09-08 01:10:35 +0300

    Add files via upload

---
+++ NTP-Inheritance(Kalıtım)-2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Static_Constructor
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+
+            
+         }                
+    }
+}
