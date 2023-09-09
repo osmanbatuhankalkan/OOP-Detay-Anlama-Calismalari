9888854764f3f0df605696977e86284f14585878
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-09-09 23:30:01 +0300

    Add files via upload

---
+++ NTP-Inheritance(Kalıtım)-3 Name Hiding Durumlarında new Operatörünün Kullanımı/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace kalitim2
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+            D d = new D();
+              d.X();           
+         }                
+    }
+    //*Atadan gelen yani A dan gelen kalıtım özelleğini, torunlarda name hidding yaptık yani oradaki ismi ezdik
+   
+    class A
+    {
+        public void X()
+        {
+
+        }                 
+    }
+    class B:A
+    {
+               
+    }
+    class C:B
+    {
+                
+    }
+    class D:C
+    {
+        public new void X()//eskiden böyle belirtilirmiş şimdi new olarak yazmak zorunda değiliz
+        {
+                         
+        }                 
+    }                                 
+}
